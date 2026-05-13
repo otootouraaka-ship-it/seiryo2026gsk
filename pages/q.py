7e64e424fa823c70f6cd02654b64c7642dfea4bf
otootouraaka-ship-it <otootouraaka@gmail.com>
2026-05-13 05:15:42 +0000

    update

--- pages/q.py
+++ pages/q.py
@@ -49,7 +49,7 @@ creds = Credentials.from_service_account_info(
 
 client = gspread.authorize(creds)
 
-SHEET_URL = "ここにURL"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1TOUV7U2uJMHM2DO08_Dqhd_babEl-XESRXKIfIqpiYE/edit?resourcekey=&gid=1281103730#gid=1281103730"
 
 sheet = client.open_by_url(
     SHEET_URL
