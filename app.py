7fde5fa71661fa7c2f7dfbf4eed86881bbda4007
otootouraaka-ship-it <otootouraaka@gmail.com>
2026-05-13 05:31:38 +0000

    update

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from google.oauth2.service_account import Credentials
 
 from streamlit_autorefresh import st_autorefresh
 
-from .background import set_bg_image
+from background import set_bg_image
 
 # =====================================
 # 自動更新
